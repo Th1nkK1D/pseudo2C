52233ce78639a5abd70312d1a73e15ed79e3fda0
Th1nkK1D <witheep@gmail.com>
2016-04-11 15:56:01 +0700

    Translator with rought structure

--- translator.c
+++ translator.c
@@ -1,4 +1,109 @@
-/* Psudo translator - Pseudo2C Project
+/* Psudocode translator - Pseudo2C Project
  * 
  * Withee Poositasai (Lookkid) 58070503429
  * Team We Must Survived */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "mainStructure.h"
+#include "dbAccess.h"
+
+
+/* Function Declaration */
+void writeStdFunction(FILE* pOut);
+void writeIndent(FILE* pOut);
+void processLine(char buffer[],FILE* pOut);
+
+int translator()
+	{
+	FILE* pIn = NULL;
+	FILE* pOut = NULL;
+	char buffer[256];
+	char inName[32];
+	char outName[32];
+
+	/* Prepare input file name */
+	printf("Input pseudocode file name : ");
+	fgets(buffer,sizeof(buffer),stdin);
+	sscanf(buffer,"%s",inName);
+
+	pIn = fopen(inName,"r");
+
+	if(pIn == NULL)
+		{
+		printf("Can't open pseudo code file: \"%s\"\n",inName);
+		return 0;
+		}
+
+	/* Prepare output file name */
+	printf("Output C file name : ");
+	fgets(buffer,sizeof(buffer),stdin);
+	sscanf(buffer,"%s",outName);
+
+	pIn = fopen(outName,"w");
+
+	if(pIn == NULL)
+		{
+		printf("Can't create C file: \"%s\"\n",outName);
+		return 0;
+		}
+
+	/* Write standard function */
+	writeStdFunction(pOut);
+
+	//push main
+
+	/* Read each line */
+	while(fgets(buffer,sizeof(buffer),pIn) != NULL)
+		{
+		if(buffer[0] == '\n')
+			{
+			fprintf(pOut,"\n");
+			}
+		else
+			{
+			if(buffer[strlen(buffer)-1] == '\n')
+				{
+				buffer[strlen(buffer)-1] = '\0';
+				}
+
+			processLine(buffer,pOut);
+			}
+		}
+
+
+
+	return 1;
+	}
+
+void writeStdFunction(FILE* pOut)
+	{
+	fprintf(pOut,"#include <stdio.h>\n");
+	fprintf(pOut,"#include <stdlib.h>\n");
+	fprintf(pOut,"#include <string.h>\n");
+	fprintf(pOut,"\n");
+	//To-Do : custom function
+	}
+
+void writeIndent(FILE* pOut)
+	{
+	int indent; //= countStack()
+	int i;
+
+	for (int i = 0; i < indent; i++)
+		{
+		fprintf(pOut,"\t");
+		}
+	}
+
+void processLine(char buffer[],FILE* pOut)
+	{
+	char key[8];
+
+	/* Key = first word */
+	sscanf(buffer,"%s",key);
+
+
+	}
