d2127528152320eb35914e090d520065598d74ed
poo1882 <poo_poo1882@hotmail.com>
2016-04-24 17:27:50 +0700

    g

--- globalAccess.c
+++ globalAccess.c
@@ -222,15 +222,15 @@ void addVariable(char in_word[],char in_type[])
 
 		if(strcmp(in_type,"int") == 0)
 			{
-			strcpy(in_symbol,"%d");
+			strcpy(in_symbol,"%%d");
 			}
 		else if(strcmp(in_type,"char") == 0)
 			{
-			strcpy(in_symbol,"%c");
+			strcpy(in_symbol,"%%c");
 			}
 		else if(strcmp(in_type,"string") == 0)
 			{
-			strcpy(in_symbol,"%s");
+			strcpy(in_symbol,"%%s");
 			}
 		in_variable = (VARIABLE_T*) calloc(1,sizeof(VARIABLE_T));
         if(in_variable == NULL)
